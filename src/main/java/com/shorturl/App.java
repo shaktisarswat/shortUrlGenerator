c11f02780f21b73787527e3a564f06edb107c28e
Shakti <shaktisarswat01@gmail.com>
2023-08-27 11:33:56 +0530

    ShortUrlGenerator Class implemented

--- src/main/java/com/shorturl/App.java
+++ src/main/java/com/shorturl/App.java
@@ -3,5 +3,6 @@ package com.shorturl;
 public class App {
     public static void main(String[] args) {
 
+
     }
 }
--- src/main/java/com/shorturl/ShortUrl.java
+++ src/main/java/com/shorturl/ShortUrl.java
@@ -3,16 +3,18 @@ package com.shorturl;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ShortUrl implements IShortUrl{
+public class ShortUrl implements IShortUrl {
     private Map<String, String> shortToLongMapping;
 
     private Map<String, String> longToShortMapping;
 
     RandomUrlGenerator randomUrlGenerator;
 
+    private Map<String, Integer> longUrlHitCount;
+
     private static final String URL = "http://short.url/";
 
-    public ShortUrl(){
+    public ShortUrl() {
 
         shortToLongMapping = new HashMap<>();
 
@@ -20,29 +22,67 @@ public class ShortUrl implements IShortUrl{
 
         randomUrlGenerator = new RandomUrlGenerator();
 
+        longUrlHitCount = new HashMap<>();
+
     }
+
     @Override
     public String registerNewUrl(String longUrl) {
-        return null;
+        if (longUrl == null) {
+            System.out.println("Error: LongUrl should not be null");
+            return null;
+        }
+        if (longToShortMapping.containsKey(longUrl)) {
+            return longToShortMapping.get(longUrl);
+        }
+        String shortUrl = randomUrlGenerator.generateRandomUrl();
+        shortUrl = URL + shortUrl;
+        longToShortMapping.put(longUrl, shortUrl);
+        shortToLongMapping.put(shortUrl, longUrl);
+        return shortUrl;
     }
 
     @Override
     public String registerNewUrl(String longUrl, String shortUrl) {
-        return null;
+        if (longUrl == null || shortUrl == null) {
+            System.out.println("Error: ShortUrl or LongUrl can't be passed as NULL");
+            return null;
+        }
+        if (longToShortMapping.containsKey(longUrl)) {
+            return null;
+        }
+
+        longToShortMapping.put(longUrl, shortUrl);
+        shortToLongMapping.put(shortUrl, longUrl);
+        return shortUrl;
     }
 
     @Override
     public String getUrl(String shortUrl) {
+        if (shortUrl == null) {
+            System.out.println("Error: ShortUrl should not be Null");
+            return null;
+        }
+        if (shortToLongMapping.containsKey(shortUrl)) {
+            return shortToLongMapping.get(shortUrl);
+        }
         return null;
     }
 
     @Override
     public Integer getHitCount(String longUrl) {
-        return null;
+        if (longToShortMapping.containsKey(longUrl)) {
+            longUrlHitCount.put(longUrl, longUrlHitCount.getOrDefault(longUrl, 0) + 1);
+            longUrlHitCount.get(longUrl);
+        }
+        return 0;
     }
 
     @Override
     public String delete(String longUrl) {
+        String shortUrl = longToShortMapping.get(longUrl);
+        longToShortMapping.remove(longUrl);
+        shortToLongMapping.remove(shortUrl);
         return null;
     }
 }
